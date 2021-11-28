2701db26031bb26b91f99c304207e5d398e3b2f5
nicklasbns <nicklasbns@gmail.com>
2021-11-28 21:35:56 +0100

    forgot to commit for some time
    also didnt do that much...

--- script1.js
+++ script1.js
@@ -7,6 +7,7 @@ canvas.onclick = click;
 
 //setup
 document.body.style.backgroundColor = "#333";
+const {width, height} = canvas
 const can = new thre.Scene();
 const cam = new thre.PerspectiveCamera(75);
 const renderer = new thre.WebGLRenderer({canvas});
@@ -15,6 +16,7 @@ renderer.shadowMap.enabled = true
 can.background = new thre.Color(0xa0a0a0)
 cam.position.z = 10;
 cam.position.y = 5;
+cam.position.x = 0;
 cam.lookAt(0, 0, 0);
 
 //grid
@@ -34,9 +36,13 @@ const crimson = new thre.MeshPhongMaterial({color: "crimson", flatShading: true,
 const wire = new thre.MeshBasicMaterial({color: "black", wireframe: true});
 
 //vars
-var time = 0;
+var time = Date.now();
+var Dtime = 0;
 var rotation = 0;
-objects = [];
+var inert = 0;
+var momentumn = 1;
+var objects = [];
+var debug = 0;
 
 
 var geo = new thre.CylinderGeometry(0.2, 0.2, 6, 16);
@@ -44,25 +50,23 @@ geo.rotateZ(Math.PI/2)
 var bar = new thre.Mesh(geo, textured);
 bar.receiveShadow = bar.castShadow = true;
 can.add(bar);
-var cyl1 = createCylinder({height: 2, width:1, x:2, z:0});
-cyl1.position.x = 2
-var cyl2 = createCylinder({height: 2, width:1, x:0, z:2});
-// cyl1.rotateOnWorldAxis(new thre.Vector3(1, 0, 0))
+// createCylinder({height: 2, width:1, x:2, z:0});
+// createCylinder({height: 2, width:1, x:-2, z:0});
 
 
-var light2 = new thre.DirectionalLight(0xffffff, 0.5, 20/*, Math.PI/2, 0, 1*/);
+var light2 = new thre.DirectionalLight(0xffffff, 0.5);
 light2.castShadow = true;
 light2.position.set(0, 5, 0);
 can.add(light2)
 var shadowGeo = new thre.PlaneGeometry(10, 10);
 var shadow = new thre.Mesh(shadowGeo, flat);
 shadow.rotateX(-Math.PI/2);
-shadow.position.y = -2.1
+shadow.position.y = -2.01
 shadow.receiveShadow = true
 can.add(shadow);
 
-let max = 5
-for (var i = 0; i < max*2; i++) {
+
+for (var i = 0, max = 5; i < max*2; i++) {
     let light = new thre.PointLight(0xffffff, 1/max, 40)
     let phi = Math.PI/2-(Math.PI/10*2)*(i>=max?-1:1);
     light.position.setFromSphericalCoords(7, phi, Math.PI*2/max*i)
@@ -72,45 +76,40 @@ for (var i = 0; i < max*2; i++) {
 }
 
 
-var point = createCylinder({height: 0.5, width: 0.1});
-point.position.y = 0.5;
+var point = createPoint(0, 0);
+point.position.set(0, 0.5, 0);
 point.castShadow = true;
 point.receiveShadow = true;
-var x = Math.PI/2, y = 0
 can.add(point);
 
 
-function click() {
-    cam.position.setX = 0;
-    raycaster.setFromCamera({x:0, y:-0.1}, cam);
+function click(event) {
+    // raycaster.setFromCamera({x:0.5, y:0.5}, cam);
+    raycaster.setFromCamera({x:event.layerX/width*2-1, y:-event.layerY/height*2+1}, cam);
     var intersects = raycaster.intersectObjects(can.children)
     console.log(intersects);
-    intersects.forEach(point => {
-        console.log(point.point);
-        let geo = new thre.SphereGeometry(0.5);
-        let mesh = new thre.Mesh(geo, crimson);
-        mesh.position.setX(point.point.x)
-        mesh.position.setY(point.point.y)
-        mesh.position.setZ(point.point.z)
-        can.add(mesh);
-    })
-    cam.position.x = 6;
-    cam.lookAt(new thre.Vector3(0, 0, 0))
+    console.log(intersects[0].point);
+    let geo = new thre.SphereGeometry(0.5);
+    let mesh = new thre.Mesh(geo, crimson);
+    mesh.position.setX(intersects[0].point.x)
+    mesh.position.setY(intersects[0].point.y)
+    mesh.position.setZ(intersects[0].point.z)
+    bar.add(mesh);
 }
 
 function loop() {
-    rotation = time/100;
-    y = Math.abs(rotation%Math.PI-0.5*Math.PI)
+    Dtime = Date.now()-time;
+    if (Dtime == 0) return
+    momentumn -= rotation*0.022
+    rotation += momentumn/Dtime/10
+    inert = 1/12*5*6**2;
+    if (debug ? momentumn > 0 : momentumn < 0) {
+        debug = !debug;
+        console.log(Math.abs(rotation).toFixed(3));
+    }
 
-    point.position = point3d(x, Math.PI/2);
-
-    // cyl1.draw(rotation);
-    // cyl1.rotateY(Math.PI/180*0.5);
-    // cam.position.x = Math.sin(rotation/3)*5;
-    // cam.position.z = Math.cos(rotation/3)*5;
-    // cam.lookAt(0, 0, 0)
     bar.rotation.y = rotation+Math.PI/2;
-    // bar.rotateX(0.1)
+    time = Date.now();
 }
 
 
@@ -127,12 +126,11 @@ function point3d(x, y) {
 }
 
 function createCylinder(options = {weight: 10, height: 2, width: 1, x:2, z:0}) {
-    unpack(options, this);
-    var geo = new thre.CylinderGeometry(width, width, height, 64);
+    var geo = new thre.CylinderGeometry(options.width, options.width, options.height, 64);
     var mesh = new thre.Mesh(geo, textured);
     mesh.receiveShadow = true;
     mesh.castShadow = true;
-    mesh.position.set(x, 0, z);
+    mesh.position.set(options.x, 0, options.z);
     bar.add(mesh);
     // var wireframe = new thre.Mesh(geo, wire);
     // mesh.add(wireframe);
@@ -153,19 +151,12 @@ function createPoint(x, y, r=1) {
     mesh.receiveShadow = true
     mesh.castShadow = true
     can.add(mesh);
-    var pos = mesh.position.setFromSphericalCoords(r, x, y)
-    objects.push(mesh)
+    mesh.position.setFromSphericalCoords(r, x, y)
     return mesh
 }
 (renderLoop = () => {
-    // cyl1.position.x = Math.sin(time/100)
     loop();
     renderer.render(can, cam);
     time++;
     requestAnimationFrame(renderLoop)
 })();
-function unpack(obj, dest) {
-    for (var key in obj) {
-        dest[key] = obj[key];
-    }
-}
