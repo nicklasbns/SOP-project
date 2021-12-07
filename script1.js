ae969633069a97d0b8bd898317237adf3fec4c94
nicklasbns (laptop) <nicklasbns@gmail.com>
2021-12-07 12:28:34 +0100

    added fancy adding and removing objects

--- script1.js
+++ script1.js
@@ -4,16 +4,19 @@ try {
 const thre = THREE;
 const canvas = document.getElementById("can");
 canvas.onclick = click;
+canvas.oncontextmenu = click;
+canvas.onmousemove = hover;
+document.getElementById("pause").onclick = pause;
 
 //setup
 document.body.style.backgroundColor = "#333";
-const {width, height} = canvas
+const {width, height} = canvas;
 const can = new thre.Scene();
 const cam = new thre.PerspectiveCamera(75);
 const renderer = new thre.WebGLRenderer({canvas});
 const raycaster = new thre.Raycaster();
 renderer.shadowMap.enabled = true
-can.background = new thre.Color(0xa0a0a0)
+can.background = new thre.Color(0xa0a0a0);
 cam.position.z = 10;
 cam.position.y = 5;
 cam.position.x = 0;
@@ -31,7 +34,9 @@ var bumps = new THREE.TextureLoader().load("https://threejs.org/examples/models/
 var plaster = new THREE.TextureLoader().load("https://thumbs.dreamstime.com/z/k-rough-plaster-roughness-texture-height-map-specular-imperfection-d-materials-black-white-hi-res-200368950.jpg");
 const textured = new thre.MeshPhongMaterial({color: "lightgray", flatShading: true, vertexColors: false, shininess: 1, bumpMap: plaster, bumpScale: 1});
 const flat = new thre.MeshPhongMaterial({color: "lightgray", flatShading: true, vertexColors: false, shininess: 1});
+const transparrent = new thre.MeshPhongMaterial({color: "white", flatShading: true, vertexColors: false, shininess: 1, transparent: true, opacity: 0});
 const crimson = new thre.MeshPhongMaterial({color: "crimson", flatShading: true, vertexColors: false, shininess: 1});
+const yellow = new thre.MeshPhongMaterial({color: new thre.Color(0.862, 0.90, 0.235), flatShading: true, vertexColors: false, shininess: 1});
 
 const wire = new thre.MeshBasicMaterial({color: "black", wireframe: true});
 
@@ -42,6 +47,8 @@ var rotation = 0;
 var inert = 0;
 var momentumn = 1;
 var objects = [];
+var highlight = 0;
+var running = true;
 var debug = 0;
 
 
@@ -62,14 +69,17 @@ var shadowGeo = new thre.PlaneGeometry(10, 10);
 var shadow = new thre.Mesh(shadowGeo, flat);
 shadow.rotateX(-Math.PI/2);
 shadow.position.y = -2.01
-shadow.receiveShadow = true
+shadow.receiveShadow = true;
+can.add(shadow);
+shadow = new thre.Mesh(shadowGeo, transparrent);
+shadow.rotateX(-Math.PI/2);
 can.add(shadow);
 
 
 for (var i = 0, max = 5; i < max*2; i++) {
-    let light = new thre.PointLight(0xffffff, 1/max, 40)
+    let light = new thre.PointLight(0xffffff, 1/max, 40);
     let phi = Math.PI/2-(Math.PI/10*2)*(i>=max?-1:1);
-    light.position.setFromSphericalCoords(7, phi, Math.PI*2/max*i)
+    light.position.setFromSphericalCoords(7, phi, Math.PI*2/max*i);
     can.add(light);
     let marker = createPoint(0, 0, 0);
     light.add(marker);
@@ -82,34 +92,66 @@ point.castShadow = true;
 point.receiveShadow = true;
 can.add(point);
 
+function pause(e) {
+    if (running) {
+        e.srcElement.innerText = "paused";
+        running = false;
+        rotation = 0;
+        momentumn  = 0;
+    } else {
+        e.srcElement.innerText = "pause";
+        running = true;
+        rotation = 0;
+        momentumn  = 1;
+    }
+
+}
 
 function click(event) {
-    // raycaster.setFromCamera({x:0.5, y:0.5}, cam);
+    if (running) return;
+    var button = event.button == 2;
     raycaster.setFromCamera({x:event.layerX/width*2-1, y:-event.layerY/height*2+1}, cam);
-    var intersects = raycaster.intersectObjects(can.children)
-    console.log(intersects);
-    console.log(intersects[0].point);
-    let geo = new thre.SphereGeometry(0.5);
-    let mesh = new thre.Mesh(geo, crimson);
-    mesh.position.setX(intersects[0].point.x)
-    mesh.position.setY(intersects[0].point.y)
-    mesh.position.setZ(intersects[0].point.z)
-    bar.add(mesh);
+    var intersects = raycaster.intersectObjects(button ? objects : [shadow]);
+    if (!intersects.length) return;
+    if (button) {
+        objects = objects.filter(e => e.uuid != intersects[0].object.uuid);
+        bar.remove(intersects[0].object);
+        event.preventDefault();
+    } else {
+        let geo = new thre.SphereGeometry(0.5);
+        let mesh = new thre.Mesh(geo, crimson);
+        mesh.position.setX(intersects[0].point.x);
+        mesh.position.setY(intersects[0].point.y);
+        mesh.position.setZ(intersects[0].point.z);
+        objects.push(mesh);
+        bar.add(mesh);
+    }
+}
+
+function hover(event) {
+    if (running) return;
+    raycaster.setFromCamera({x:event.layerX/width*2-1, y:-event.layerY/height*2+1}, cam);
+    var intersects = raycaster.intersectObjects(objects);
+    if (!intersects.length) return highlight.material = crimson;
+    if (highlight != intersects[0].object || !highlight) {
+        highlight.material = crimson;
+        highlight = intersects[0].object
+    }
+    highlight.material = yellow;
 }
 
 function loop() {
     Dtime = Date.now()-time;
-    if (Dtime == 0) return
-    momentumn -= rotation*0.022
-    rotation += momentumn/Dtime/10
+    time = Date.now();
+    if (Dtime == 0 || Dtime > 500) return
+    momentumn -= rotation*0.022;
+    rotation += momentumn*Dtime/300;
     inert = 1/12*5*6**2;
     if (debug ? momentumn > 0 : momentumn < 0) {
         debug = !debug;
         console.log(Math.abs(rotation).toFixed(3));
     }
-
-    bar.rotation.y = rotation+Math.PI/2;
-    time = Date.now();
+    bar.rotation.y = rotation;
 }
 
 
@@ -142,7 +184,7 @@ function createCylinder(options = {weight: 10, height: 2, width: 1, x:2, z:0}) {
         mesh.setRotationFromAxisAngle(new thre.Vector3(0, 1, 0).add(new thre.Vector3(0, 0, 0)).setLength(1), rotation)
         // mesh.setRotationFromAxisAngle(new thre.Vector3(1, 0, 0), rotation/2)
     }
-    objects.push(mesh)
+    // objects.push(mesh)
     return mesh
 }
 function createPoint(x, y, r=1) {
@@ -151,7 +193,7 @@ function createPoint(x, y, r=1) {
     mesh.receiveShadow = true
     mesh.castShadow = true
     can.add(mesh);
-    mesh.position.setFromSphericalCoords(r, x, y)
+    mesh.position.setFromSphericalCoords(r, x, y);
     return mesh
 }
 (renderLoop = () => {
